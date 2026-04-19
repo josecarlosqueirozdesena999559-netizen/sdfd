0458be09ee1ce9f9efdc6a5dea982914bbfb00be
Seu Nome <seu-email-do-github@exemplo.com>
2026-04-19 16:20:31 -0300

    Refina fundo da tela de login

--- RequisiPlusApp/Views/LoginView.swift
+++ RequisiPlusApp/Views/LoginView.swift
@@ -11,7 +11,7 @@ struct LoginView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                AppTheme.background
+                AppTheme.backgroundGradient
                     .ignoresSafeArea()
 
                 VStack(spacing: 0) {
